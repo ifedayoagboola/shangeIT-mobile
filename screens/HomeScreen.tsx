52d4f71b893a05ce1933c12f90cd5124e3d2c337
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-03-12 23:59:39 +0000

    resolved tailwind issue

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,13 @@
+// import { StatusBar } from "expo-status-bar";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text className="flex-1 items-center justify-center bg-red-500 text-2xl">
-        Let us build shange-it.
-      </Text>
-      <StatusBar style="auto" />
+    <SafeAreaView className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-xl">Let us build shange-it.</Text>
+      {/* <StatusBar /> */}
     </SafeAreaView>
   );
 };
