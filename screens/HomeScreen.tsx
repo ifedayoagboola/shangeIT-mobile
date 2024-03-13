fae0801b1078cf430e1dda12dc45076ba62500fe
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-03-13 00:39:39 +0000

    replicated logo screen figma design

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,24 @@
-// import { StatusBar } from "expo-status-bar";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-xl">Let us build shange-it.</Text>
-      {/* <StatusBar /> */}
+    <SafeAreaView
+      className="flex-1 items-center justify-center"
+      style={styles.container}
+    >
+      <Image
+        style={{ width: 100, height: 100, resizeMode: "contain" }}
+        source={require("../assets/logo2.png")}
+      />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#2934D0",
+  },
+});
 
 export default HomeScreen;
