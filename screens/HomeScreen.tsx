5b7fd968d5a9156c41d3f80c0b948ebacfb49b21
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-03-15 23:49:59 +0000

    time out for logo screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
+  const [showLogoScreen, setShowLogoScreen] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLogoScreen(false);
+    }, 700); // 2000 milliseconds = 2 seconds
+
+    return () => clearTimeout(timeout);
+  }, []); // Runs once on component mount
+
   return (
-    <SafeAreaView
-      className="flex-1 items-center justify-center"
-      style={styles.container}
-    >
-      <Image
-        style={{ width: 100, height: 100, resizeMode: "contain" }}
-        source={require("../assets/logo2.png")}
-      />
-    </SafeAreaView>
+    <>
+      {showLogoScreen ? (
+        <SafeAreaView style={styles.container}>
+          <Image
+            style={{ width: 100, height: 100, resizeMode: "contain" }}
+            source={require("../assets/logo2.png")}
+          />
+        </SafeAreaView>
+      ) : (
+        <SafeAreaView style={styles.container2}>
+          <Text className="text-black text-lg">Move to the next screen!</Text>
+        </SafeAreaView>
+      )}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#2934D0",
+  },
+  container2: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 20,
+    color: "#FFFFFF",
   },
 });
 
