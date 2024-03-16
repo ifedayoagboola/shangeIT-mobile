844fd5484d94b899d52741ad132516b1343516ab
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-03-16 23:59:35 +0000

    pushing daily code

---
+++ components/NavOptions.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FlatList } from "react-native";
+const data = [
+  {
+    id: "1",
+    title: "dfnjk",
+    image: "dfcjnk",
+    screen: "sdkhnjk",
+  },
+];
+
+const NavOptions = () => {
+  return <FlatList />;
+};
+
+export default NavOptions;
