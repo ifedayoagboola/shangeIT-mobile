4e669ce529b71c045f3730d415184d9136916a0b
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-03-18 06:06:05 +0000

    touchable opacity for header icons

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -12,19 +12,21 @@ const NavHeader = () => {
   return (
     <SafeAreaView>
       <View className="flex flex-row items-center justify-between">
-        <Image
-          source={require("../assets/face.png")}
-          alt="burger illustration"
-          style={{ width: 50, height: 50 }}
-        />
+        <TouchableOpacity>
+          <Image
+            source={require("../assets/face.png")}
+            alt="burger illustration"
+            style={{ width: 50, height: 50 }}
+          />
+        </TouchableOpacity>
         <Text className="text-2xl font-semibold">Home</Text>
-        <View className="border border-gray-200 p-4 rounded-2xl">
+        <TouchableOpacity className="border border-gray-200 p-4 rounded-2xl">
           <Image
             source={require("../assets/outline.png")}
             alt="burger illustration"
             style={{ width: 17, height: 20 }}
           />
-        </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
