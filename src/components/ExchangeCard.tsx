98c5e71940e958008f5bdd516f51a2cff15865e4
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-08-03 17:05:07 +0100

    daily puah

--- src/components/ExchangeCard.tsx
+++ src/components/ExchangeCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, Linking, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import Link from "./Link";
 
 const ExchangeCard = () => {
